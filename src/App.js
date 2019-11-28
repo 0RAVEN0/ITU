f834d9bdac844745bba5525ceab28dcf4dd28e42
0RAVEN0 <43756842+0RAVEN0@users.noreply.github.com>
2019-11-28 22:53:55 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+    render() {
+      return(
+        <div class="App">
+        <div class="Info_container">
+          <div class="Title">
+            PEXESO
+          </div>
+          <div class="Info">
+            TIME
+          </div>
+          <div class="Info">
+            FLIPS
+          </div>
+        </div>
+        <div class="App_container">
+          <div class="Card">
+            <img class="Card-front" src="./caka.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./caka.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./lezi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./lezi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./lubi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./lubi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./odpociva.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./odpociva.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./skavou.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./skavou.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./tesisa.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./tesisa.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./velmisatesi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./velmisatesi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./zaselubi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+          <div class="Card">
+            <img class="Card-front" src="./zaselubi.svg" alt="Caka"/>
+            <img class="Card-back" src="./logo.svg" alt="Logo"/>
+          </div>
+        </div>
+        </div>
+      );
+    }
+}
+
+export default App;
